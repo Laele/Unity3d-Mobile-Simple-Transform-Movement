d234e622dc28facbaaa04411e70aba12de3beabb
Luis Soriano <49929267+Laele@users.noreply.github.com>
2020-09-20 19:27:10 -0500

    Initial project

---
+++ 3d Mobile Simple Transform Movement/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void RightMove(){
+        this.gameObject.transform.position = new Vector3(gameObject.transform.position.x,0,this.gameObject.transform.position.z-1);
+        print(this.gameObject.transform.position);
+    }
+    
+    public void LeftMove(){
+        this.gameObject.transform.position = new Vector3(gameObject.transform.position.x,0,this.gameObject.transform.position.z+1);
+        print(this.gameObject.transform.position);
+        
+    }
+    
+    public void UpMove(){
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x+1,0,this.gameObject.transform.position.z);
+        print(this.gameObject.transform.position);
+    }
+
+    public void BackMove(){
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x-1,0,this.gameObject.transform.position.z);
+        print(this.gameObject.transform.position);
+    }
+}
